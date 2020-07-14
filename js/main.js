f0ce1412d7fa1fe950ada6326d0c147b7100f639
HOBIRON <yudainepnep@icloud.com>
2020-07-14 15:01:35 +0900

    顔の輪郭部分画像の表示を実装

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
-// 座標の背景画像を描画する
-var DrawCoordinateImage = function (ctx) {
+var CoordinateCanvas = document.getElementById('coordinate');
+var cctx = CoordinateCanvas.getContext('2d');
+var FaceCanvas = document.getElementById('emotion-face');
+var fctx = FaceCanvas.getContext('2d');
+// ドラッグ可能な座標の背景画像を描画
+var DrawCoordinateImage = function () {
     var background = new Image();
     var canvas_width = 400;
     var canvas_hegiht = 400;
     var imageURL = "/Users/kawakamiyuudai/研究プロジェクト/EmotionExpressionSystem/canvas-project/Images/Cordinate.png";
     background.src = imageURL;
     background.onload = function () {
-        if (ctx != null) {
-            ctx.drawImage(background, 0, 0, canvas_width, background.height * canvas_width / background.width);
+        if (cctx != null) {
+            cctx.drawImage(background, 0, 0, canvas_width, background.height * canvas_width / background.width);
         }
     };
 };
+// ドラッグで作成する顔の輪郭部分を表示
+var DrowBaseFaceImage = function () {
+    var background = new Image();
+    var canvas_width = 200;
+    var canvas_hegiht = 200;
+    var imageURL = "/Users/kawakamiyuudai/研究プロジェクト/EmotionExpressionSystem/canvas-project/Images/BaseFace.png";
+    background.src = imageURL;
+    background.onload = function () {
+        if (fctx != null) {
+            fctx.drawImage(background, 0, 0, canvas_width, background.height * canvas_width / background.width);
+        }
+    };
+};
+var Init = function () {
+    DrowBaseFaceImage();
+    DrawCoordinateImage();
+};
 var main = (function () {
-    var canvas = document.getElementById('coordinate');
-    var ctx = canvas.getContext('2d');
-    DrawCoordinateImage(ctx);
+    Init();
 })();
 // ページ読み込み後に走る
 window.onload = function () {
--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,11 @@
-// 座標の背景画像を描画
-const DrawCoordinateImage = (ctx: CanvasRenderingContext2D | null):void => {
+const CoordinateCanvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('coordinate');
+const cctx:CanvasRenderingContext2D | null = CoordinateCanvas.getContext('2d');
+
+const FaceCanvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('emotion-face');
+const fctx:CanvasRenderingContext2D | null = FaceCanvas.getContext('2d');
+
+// ドラッグ可能な座標の背景画像を描画
+const DrawCoordinateImage = ():void => {
     let background: HTMLImageElement = new Image();
     const canvas_width: number = 400;
     const canvas_hegiht: number = 400;
@@ -7,20 +13,37 @@ const DrawCoordinateImage = (ctx: CanvasRenderingContext2D | null):void => {
 
     background.src = imageURL;
     background.onload = () => {
-        if (ctx != null){
-            ctx.drawImage(background,0,0,canvas_width, background.height * canvas_width / background.width);
+        if (cctx != null){
+            cctx.drawImage(background,0,0,canvas_width, background.height * canvas_width / background.width);
         }
     }
-}
+};
+
+// ドラッグで作成する顔の輪郭部分を表示
+const DrowBaseFaceImage = ():void => {
+    let background: HTMLImageElement = new Image();
+    const canvas_width: number = 200;
+    const canvas_hegiht: number = 200;
+    const imageURL: string = "/Users/kawakamiyuudai/研究プロジェクト/EmotionExpressionSystem/canvas-project/Images/BaseFace.png";
+
+    background.src = imageURL;
+    background.onload = () => {
+        if (fctx != null){
+            fctx.drawImage(background,0,0,canvas_width, background.height * canvas_width / background.width);
+        }
+    }
+};
+
+const Init = ():void => {
+    DrowBaseFaceImage();
+    DrawCoordinateImage();
+};
 
 const main = (() => {
-    const canvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('coordinate');
-    const ctx:CanvasRenderingContext2D | null = canvas.getContext('2d');
-
-    DrawCoordinateImage(ctx);
+    Init();
 })();
 
 // ページ読み込み後に走る
 window.onload = () => {
     console.log("Read page!!");
-}
+};
