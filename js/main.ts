54d224fb9e9d91cd127db989749ebc70d4ccafd2
HOBIRON <yudainepnep@icloud.com>
2020-10-11 18:49:31 +0900

    Definen color on coordinate

--- js/main.ts
+++ js/main.ts
@@ -28,6 +28,30 @@ const PLEASURE = {
   b: 128,
 };
 
+const ANGRY_HAPPY = {
+  r: (ANGRY.r + HAPPY.r) / 2,
+  g: (ANGRY.g + HAPPY.g) / 2,
+  b: (ANGRY.b + HAPPY.b) / 2,
+};
+
+const HAPPY_PLESSURE = {
+  r: (PLEASURE.r + HAPPY.r) / 2,
+  g: (PLEASURE.g + HAPPY.g) / 2,
+  b: (PLEASURE.b + HAPPY.b) / 2,
+}
+
+const PLESSURE_SAD = {
+  r: (PLEASURE.r + SAD.r) / 2,
+  g: (PLEASURE.g + SAD.g) / 2,
+  b: (PLEASURE.b + SAD.b) / 2,
+}
+
+const SAD_ANGRY = {
+  r: (ANGRY.r + SAD.r) / 2,
+  g: (ANGRY.g + SAD.g) / 2,
+  b: (ANGRY.b + SAD.b) / 2,
+}
+
 interface Mouse {
   startPosX: number;
   startPosY: number;
